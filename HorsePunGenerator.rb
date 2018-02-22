846df9fcdc1e2698fd7c91c8de586b4726844381
Danny Carnage <danbuckley1985@gmail.com>
2018-02-22 08:04:30 +0000

    Initial commit
    It will take a sentence and a list of words and will substitute them into the sentence based on similarity

---
+++ HorsePunGenerator.rb
@@ -0,0 +1,20 @@
+require 'rhymes'
+require 'text'
+sentence = 'May the force be with you'
+words = ['horse', 'house', 'porsche', 'force', 'coarse', 'course', 'recourse', 'hoof', 'hooves', 'horses']
+metawords = []
+output = []
+white = Text::WhiteSimilarity.new
+words.each do |w|
+metawords.push(Text::Metaphone.metaphone(w))
+end
+sentence.split.each do |s|
+metawords.each do |m|
+if white.similarity(m, Text::Metaphone.metaphone(s)) > 0.3 then
+tempsentence = sentence.split
+tempsentence[sentence.split.index(s)] = words[metawords.index(m)]
+output.push tempsentence.join(" ")
+end
+end
+end
+puts output
